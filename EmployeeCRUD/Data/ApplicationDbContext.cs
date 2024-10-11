2ccc034118c0399f5b431a3e40e82055368321a2
Dhruval Maniyar <maniyardhruval1290@gmail.com>
2024-10-11 15:40:43 +0530

    Update Readme file

---
+++ EmployeeCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
