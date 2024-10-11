9af44fed4c788fffb1c574668b6df359b0f75686
Dhruval Maniyar <maniyardhruval1290@gmail.com>
2024-10-11 14:30:00 +0530

    Delete FirstApp directory

--- FirstApp/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstApp.Models
-{
-    public class Person
-    {
-        public string Name { get; set; } = String.Empty;
-        public int Age { get; set; }
-        public string Location { get; set; } = String.Empty;
-    }
-}
